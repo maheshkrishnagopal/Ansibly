796df72b6d89ef970ca4eded5edc2aaab532cc2b
Idiot_AG <maheshkrishnagopal@gmail.com>
2019-10-11 20:31:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,5 +21,11 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.4'
+    python_requires='>=3.4',
+    install_requires=[
+        'pymongo',
+        'platform',
+        'sys',
+        'os',
+    ]
 )
